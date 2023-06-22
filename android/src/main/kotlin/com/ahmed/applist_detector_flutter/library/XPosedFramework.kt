92ecd3be9322919f6082c1e216705c9567e75150
AhmedMahmoud <am303737@gmail.com>
2023-06-22 09:49:09 +0300

    add more checks for Xposed Framework

--- android/src/main/kotlin/com/ahmed/applist_detector_flutter/library/XPosedFramework.kt
+++ android/src/main/kotlin/com/ahmed/applist_detector_flutter/library/XPosedFramework.kt
@@ -3,9 +3,15 @@ package com.ahmed.applist_detector_flutter.library
 import android.annotation.SuppressLint
 import android.content.Context
 import android.content.pm.PackageManager
+import android.os.Process
+import android.util.Log
+import java.io.BufferedReader
+import java.io.FileReader
 
-// Copied from: https://github.com/GantMan/jail-monkey
-// Copied from: https://github.com/weikaizhi/AntiDebug
+
+// Borrowed from: https://github.com/GantMan/jail-monkey
+// Borrowed from: https://github.com/weikaizhi/AntiDebug
+// Borrowed from: https://d3adend.org/blog/posts/android-anti-hooking-techniques-in-java/
 class XPosedFramework(context: Context) : IDetector(context) {
     override val name = "XPosed Framework"
 
@@ -15,7 +21,7 @@ class XPosedFramework(context: Context) : IDetector(context) {
         "de.robv.android.xposed"
     )
 
-    private fun advancedHookDetection(): Result {
+    private fun stackChecking(): Result {
         try {
             throw Exception()
         } catch (e: Exception) {
@@ -49,6 +55,36 @@ class XPosedFramework(context: Context) : IDetector(context) {
         return Result.NOT_FOUND
     }
 
+    private fun jarChecks(): Result {
+        val libraries = mutableSetOf<String>()
+        try {
+            val reader = BufferedReader(FileReader("/proc/${Process.myPid()}/maps"))
+            while(true) {
+                val line = reader.readLine() ?: break
+                if (line.endsWith(".jar")) {
+                    val begin = line.indexOf(' ') + 1
+                    val end = line.lastIndexOf(' ')
+                    libraries.add(line.substring(begin, end))
+                }
+            }
+            reader.close()
+        } catch (e: Exception) {
+            Log.e("XPosedFramework.jarChecks()", "Checking jar failed", e)
+            return Result.SUSPICIOUS
+        }
+
+        for (library in libraries) {
+            if (library.contains("com.saurik.substrate")) {
+                return Result.FOUND
+            }
+            if (library.contains("XposedBridge.jar")) {
+                return Result.FOUND
+            }
+        }
+
+        return Result.NOT_FOUND
+    }
+
     @SuppressLint("QueryPermissionsNeeded")
     override fun run(packages: Collection<String>?, detail: Detail?): Result {
         var result = Result.NOT_FOUND
@@ -63,7 +99,10 @@ class XPosedFramework(context: Context) : IDetector(context) {
                 add(packageManager.getApplicationLabel(applicationInfo) as String to Result.FOUND)
             }
         }
-        add("Advanced Hook Detection" to advancedHookDetection())
+
+        add("Stack Check" to stackChecking())
+        add("Jar Check" to jarChecks())
+
         return result
     }
 
